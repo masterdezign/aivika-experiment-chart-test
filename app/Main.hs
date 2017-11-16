f6f25475f2dffb04f99dd17a1a9f1565a08b423a
Bogdan <masterdezign@gmail.com>
2017-11-16 13:26:40 +0100

    Use code from the Aivika manual (Section 1.4)

--- app/Main.hs
+++ app/Main.hs
@@ -1,27 +1,13 @@
-
--- To run, package aivika-experiment-diagrams must be installed.
-
 import Simulation.Aivika.Experiment
 import Simulation.Aivika.Experiment.Chart
 import Simulation.Aivika.Experiment.Chart.Backend.Diagrams
-
 import Graphics.Rendering.Chart.Backend.Diagrams
-
-import Web.Browser
-
-import System.FilePath
-import System.Directory
 
 import Model
 import Experiment
 
-filePath = WritableFilePath "experiment"
-
-main = 
+main =
   do fonts <- loadCommonFonts
-     let renderer = DiagramsRenderer SVG (return fonts)
-     runExperimentParallel experiment generators (WebPageRenderer renderer filePath) model
-     dir <- getCurrentDirectory
-     let index = dir </> "experiment" </> "index.html"
-     _ <- openBrowser index
-     return ()
+     let r0 = DiagramsRenderer SVG (return fonts)
+         r  = WebPageRenderer r0 experimentFilePath
+     runExperimentParallel experiment generators r model
