31be3c885e05599e696ad785041646c7912f9452
Bogdan <masterdezign@gmail.com>
2017-11-17 20:07:28 +0100

    Apply the hack from David

--- app/Model.hs
+++ app/Model.hs
@@ -2,18 +2,41 @@
 {-# LANGUAGE RecursiveDo #-}
 module Model where
 
+import Data.Array
+
 import Simulation.Aivika
 import Simulation.Aivika.SystemDynamics
 import Simulation.Aivika.Experiment
 
+import Simulation.Aivika.Internal.Specs
+import Simulation.Aivika.Internal.Parameter
+import Simulation.Aivika.Internal.Dynamics
+
 -- Mackey-Glass delay equation
 model :: Simulation Results
 model =
-  mdo xTau <- delayI x tauD 0.2
-      x <- integ (beta * xTau / (1 + xTau^10) - gamma * x) 0.2
+  mdo -- xTau <- delayI x tauD 0.2
+      x <- integ (beta * xTauPrecise / (1 + xTauPrecise^10) - gamma * x) 0.2
       let gamma = 0.1
           beta = 0.2
-          tauD = 17  -- Delay time
+          -- tauD = 17  -- Delay time
+
+      points <- liftParameter $
+        Parameter $ \r ->
+        do let xs = integPoints r
+               bnds = integIterationBnds (runSpecs r)
+           return $
+             listArray bnds xs
+
+      let xTauPrecise =
+            Dynamics $ \p ->
+              if pointIteration p > 170
+              then let n  = pointIteration p
+                       ph = pointPhase p
+                   in if ph < 3
+                      then invokeDynamics (points ! (n - 170)) x
+                      else invokeDynamics (points ! (n - 169)) x
+              else return 0.2
 
       return $ results
         [resultSource "t" "time" time,
