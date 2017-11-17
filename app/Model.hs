4f03774aa3ad02eb1794ce308444b4813c9e4b70
Bogdan <masterdezign@gmail.com>
2017-11-17 20:58:13 +0100

    Patch the Ikeda model

--- app/Model.hs
+++ app/Model.hs
@@ -20,12 +20,13 @@ model =
       let gamma = 0.1
           beta = 0.2
           -- tauD = 17  -- Delay time
+          k = 170  -- Samples per delay
 
       let xTauPrecise =
             Dynamics $ \p ->
-            if pointIteration p > 170
+            if pointIteration p > k
             then let n = pointIteration p
-                 in invokeDynamics (points ! (n - 170)) x
+                 in invokeDynamics (points ! (n - k)) x
             else return 0.2
 
       points <- liftParameter $
@@ -41,12 +42,27 @@ model =
 
 modelIkeda :: Simulation Results
 modelIkeda =
-  mdo xTau <- delayI x tauD 0.2
-      x <- integ ((-x + beta * 0.5 * (1 - (cos $ 2 * (xTau + phi0))))/epsilon) 0.2
+  mdo -- xTau <- delayI x tauD 0.2
+      x <- integ ((-x + beta * 0.5 * (1 - (cos $ 2 * (xTauPrecise + phi0))))/epsilon) 0.2
       let phi0 = 0.2
           beta = 3
           epsilon = 0.005
-          tauD = 1  -- Delay time
+          -- tauD = 1  -- Delay time
+          k = 2000
+
+      points <- liftParameter $
+        Parameter $ \r ->
+        do let xs = integPoints r
+               bnds = integIterationBnds (runSpecs r)
+           return $
+             listArray bnds xs
+
+      let xTauPrecise =
+            Dynamics $ \p ->
+            if pointIteration p > k
+            then let n = pointIteration p
+                 in invokeDynamics (points ! (n - k)) x
+            else return 0.2
 
       return $ results
         [resultSource "t" "time" time,
